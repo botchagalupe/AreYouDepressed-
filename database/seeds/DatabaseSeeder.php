9942382303ba3f0c3f83c90ab8748769c1c6e68a
Evan Smith <esincork@gmail.com>
2015-12-05 00:42:58 +0000

    Seed basic settings from env file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\Setting;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,37 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call('UserTableSeeder');
+
+
+        Setting::create([
+            'name' => 'password', 
+            'setting' => Hash::make(env('ADMIN_PASSWORD'))
+        ]);
+
+        Setting::create([
+            'name' => 'name', 
+            'setting' => env('NAME')
+        ]);
+
+        Setting::create([
+            'name' => 'email', 
+            'setting' => env('ADMIN_EMAIL')
+        ]);
+
+        Setting::create([
+            'name' => 'email_period_lower_bound', 
+            'setting' => 22
+        ]);
+
+        Setting::create([
+            'name' => 'email_period_upper_bound', 
+            'setting' => 8
+        ]);
+
+        Setting::create([
+            'name' => 'last_email', 
+            'setting' => 0
+        ]);
 
         Model::reguard();
     }
